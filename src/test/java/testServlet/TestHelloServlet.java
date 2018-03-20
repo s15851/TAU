473b32abf651b9088fecef268e6a92fbfced6ab9
s15851 <s15851@pjwstk.edu.pl>
2018-03-20 19:21:00 +0100

    Initial Commit

---
+++ src/test/java/testServlet/TestHelloServlet.java
@@ -0,0 +1,56 @@
+package testServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import servlets.HelloServlet;
+
+public class TestHelloServlet extends Mockito {
+	@Test
+	public void servlet_should_not_greet_the_user_if_the_name_is_null() throws IOException {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		HelloServlet servlet = new HelloServlet();
+		
+		when(request.getParameter("name")).thenReturn(null);
+		
+		servlet.doPost(request,  response);
+		
+		verify(response).sendRedirect("/");
+	}
+	
+	@Test
+	public void servlet_should_not_greet_the_user_if_the_name_is_empty() throws IOException {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		HelloServlet servlet = new HelloServlet();
+		
+		when(request.getParameter("name")).thenReturn("");
+		
+		servlet.doPost(request,  response);
+		
+		verify(response).sendRedirect("/");
+	}
+	
+	@Test
+	public void servlet_should_not_greet_the_user_if_the_name_is_provided() throws IOException {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		HttpServletResponse response = mock(HttpServletResponse.class);
+		PrintWriter writer = mock(PrintWriter.class);
+		
+		when(request.getParameter("name")).thenReturn("Jan");
+		when(response.getWriter()).thenReturn(writer);
+		
+		new HelloServlet().doPost(request,  response);
+		
+		verify(writer).println("<h1>Hello Jan</h1>");
+	}
+}
